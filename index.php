c2157323d2af8d80bc8c9882fa4823ed697600a0
jyoti-gautam <68596502+jyoti-gautam@users.noreply.github.com>
2024-10-05 14:15:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP 2028 TESTING PAGE</h1>
-		<p><b> openshift DO280 SESSION </b></p>
+	<h1>PHP 2024 TESTING PAGE</h1>
+		<p><b> openshift DO280 SESSION Weekends </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION4 </h2> 
